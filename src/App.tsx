64faf6d2c08f4c0cc296d69c0a0aba0a27ba324e
Melody Universe <melody.universe.2021@gmail.com>
2023-08-07 17:40:06 -0700

    brought back vite logo for fun

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import reactLogo from "./assets/computost.svg";
-import viteLogo from "/vite.svg";
+import reactLogo from "/computost.svg";
+import viteLogo from "./assets/vite.svg";
 import "./App.css";
 
 function App() {
