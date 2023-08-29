609d43afd1d930ceb8ce3611b9df06182d3c5e03
D365Dalton <59377234+D365Dalton@users.noreply.github.com>
2023-08-29 15:30:43 -0600

    Added dart theme and menu drawer

--- src/Template.tsx
+++ src/Template.tsx
@@ -1,44 +1,73 @@
-import { FunctionComponent, PropsWithChildren } from "react";
+import { FunctionComponent, PropsWithChildren, useState } from "react";
 import { ReactComponent as ComputostLogo } from "./assets/computost.svg";
-import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
-import { Menu as MenuIcon } from "@mui/icons-material";
+import { AppBar, Box, Drawer, IconButton, ListItem, ListItemButton, ListItemIcon,
+  ListItemText, ThemeProvider, Toolbar, Typography, createTheme } from "@mui/material";
+import { Book, Menu, Home } from "@mui/icons-material";
 
 export const Template: FunctionComponent<PropsWithChildren> = ({
   children,
 }) => {
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+  });
+  const data = [
+    { name: "Home", icon: <Home />, ref: "/" },
+    { name: "Blog", icon: <Book />, ref: "/blog" }
+  ];
+  const [open, setOpen] = useState(false);
+  const getList = () => (
+    <div onClick={() => setOpen(false)}>
+        {data.map((item, index) => (
+          <ListItem key={index}>
+            <ListItemIcon>{item.icon}</ListItemIcon>
+            <ListItemButton component="a" href={item.ref} >
+              <ListItemText primary={item.name} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+    </div>
+  );
   return (
     <>
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <ComputostLogo
-              title="Computost Consulting"
-              style={{
-                fill: "#fcecd1",
-                height: "1em",
-                marginRight: "0.25em",
-              }}
-            />
-            <Typography
-              href="/"
-              variant="h6"
-              component="a"
-              sx={{ color: "inherit", flexGrow: 1 }}
-            >
-              Computost Consulting
-            </Typography>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-            >
-              <MenuIcon />
-            </IconButton>
-          </Toolbar>
-        </AppBar>
-      </Box>
+      <ThemeProvider theme={darkTheme}>
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="static">
+            <Toolbar>
+              <ComputostLogo
+                title="Computost Consulting"
+                style={{
+                  fill: "#fcecd1",
+                  height: "2em",
+                  marginRight: "0.25em",
+                }}
+              />
+              <Typography
+                href="/"
+                variant="h6"
+                component="a"
+                sx={{ color: "inherit", flexGrow: 1 }}
+              >
+                Computost Consulting
+              </Typography>
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+                onClick={() => {setOpen(true)}}
+              >
+                <Menu />
+              </IconButton>
+            </Toolbar>
+          </AppBar>
+          <Drawer open={open} anchor={"right"} onClose={() => setOpen(false)}>
+            {getList()}
+          </Drawer>
+        </Box>
+      </ThemeProvider>
       <main style={{ padding: 8 }}>{children}</main>
     </>
   );
